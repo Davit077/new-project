0a92be98e32db85a798d3e1d99fac3e454d768ee
Davit077 <jachvadze2007@gmail.com>
2025-02-27 16:06:43 +0400

    up

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
-document.getElementById("close-button").addEventListener("click", function() {
-    document.getElementById("demo-mode").style.display = "none";
-    window.location.href = "indexhtml";
-});
+
+ const products = [
+    { name: "i'm product", price: 19.99, image: "https://static.wixstatic.com/media/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg" },
+    { name: "i'm product", price: 89.99, image: "https://static.wixstatic.com/media/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg/v1/fill/w_779,h_779,al_c,q_85,usm_0.66_1.00_0.01/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg" },
+    { name: "i'm product", price: 39.99, image: "https://static.wixstatic.com/media/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg" },
+    { name: "i'm product", price: 29.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 25.99, image: "https://static.wixstatic.com/media/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg" },
+    { name: "i'm product", price: 19.99, image: "https://static.wixstatic.com/media/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg/v1/fill/w_779,h_779,al_c,q_85,usm_0.66_1.00_0.01/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg" },
+    { name: "i'm product", price: 69.99, image: "https://static.wixstatic.com/media/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg" },
+    { name: "i'm product", price: 59.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 34.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 22.99, image: "https://static.wixstatic.com/media/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg" },
+    { name: "i'm product", price: 79.99, image: "https://static.wixstatic.com/media/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg/v1/fill/w_779,h_779,al_c,q_85,usm_0.66_1.00_0.01/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg" },
+    { name: "i'm product", price: 74.99, image: "https://static.wixstatic.com/media/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg" },
+    { name: "i'm product", price: 45.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 59.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 49.99, image: "https://static.wixstatic.com/media/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg" },
+    { name: "i'm product", price: 29.99, image: "https://static.wixstatic.com/media/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_a220b0ace7c14f89b580dceded35f7b6.jpg" },
+    { name: "i'm product", price: 199.99, image: "https://static.wixstatic.com/media/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_1a0f12c2b7d44080a23e2642cae13b74.jpg" },
+    { name: "i'm product", price: 89.99, image: "https://static.wixstatic.com/media/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg/v1/fill/w_779,h_779,al_c,q_85,usm_0.66_1.00_0.01/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg" },
+    { name: "i'm product", price: 59.99, image: "https://static.wixstatic.com/media/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg/v1/fill/w_779,h_779,al_c,q_85,usm_0.66_1.00_0.01/9c608a_8ac1adb0a50749a5aa72cd0181945cd0.jpg" },
+    { name: "i'm product", price: 49.99, image: "https://static.wixstatic.com/media/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg/v1/fill/w_667,h_667,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/9c608a_8db6307d7c7b4b70b675a59fd036787e.jpg" }
+ ];
+ const productList = document.getElementById("product-list");
+ products.forEach(product => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+ <img src="${product.image}" alt="${product.name}">
+ <h3>${product.name}</h3>
+ <p>$${product.price.toFixed(2)}</p>
+    `;
+    productList.appendChild(productDiv);
+ });
+ fetch('product.json')
+ .then(response => response.json())
+ .then(product => {   })
+ .catch(error => console.error("error fetching data:", error));
