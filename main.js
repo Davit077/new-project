d3cf63ec3971653ec08eea7cb68efe9e0c015892
Davit077 <jachvadze2007@gmail.com>
2025-03-04 13:28:44 +0400

    up

--- main.js
+++ main.js
@@ -36,11 +36,4 @@
  .then(response => response.json())
  .then(product => {   })
  .catch(error => console.error("error fetching data:", error));
-
- document.addEventListener("DOMContentLoaded", function () {
-   const burger = document.getElementById("burger-menu");
-   const nav = document.getElementById("nav-links");
-   burger.onclick = function () {
-       nav.classList.toggle("active");
-   };
-});
+ 
