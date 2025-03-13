2389518f0affa7ece48d7b30316955f5db9ddcf8
Davit077 <jachvadze2007@gmail.com>
2025-03-13 12:57:55 +0400

    correct

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
-
-fetch('https://67c03334b9d02a9f2248b967.mockapi.io/mock-api/card')
-.then(response =>{
-    if(!response.ok){
-        throw new Error ('response is not ok')
+fetch("https://67c03334b9d02a9f2248b967.mockapi.io/mock-api/card")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("response is not ok");
     }
-    return response.json()
-}
-    
-)
-.then(data =>{
-    const toyContainer = document.getElementById('product-list')
-    data.forEach(toy =>{
-        const toyCard = document.createElement('div')
-        toyCard.classList.add('toy-card')
+    return response.json();
+  })
+  .then((data) => {
+    const toyContainer = document.getElementById("product-list");
+    data.forEach((toy) => {
+      const toyCard = document.createElement("div");
+      toyCard.classList.add("toy-card");
 
-
-        toyCard.innerHTML=`
+      toyCard.innerHTML = `
         <img src="${toy.img}" alt="${toy.name}">
         <h2>${toy.name} </h2>
         <p>Price: $${toy.price} </p>
-        `
-        toyContainer.appendChild(toyCard)
-    })
-})
-.catch(error =>{
-    console.error("შეცდომა", error)
-})
+        `;
+      toyContainer.appendChild(toyCard);
+    });
+  })
+  .catch((error) => {
+    console.error("შეცდომა", error);
+  });
+
+fetch("https://67c03334b9d02a9f2248b967.mockapi.io/mock-api/card-hover")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Response is not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const toyContainer = document.getElementById("hov");
+    data.forEach((toy) => {
+      const toyCard = document.createElement("div");
+      toyCard.classList.add("card-hover");
+      toyCard.innerHTML = `
+        <div class="photo">
+        <img id="hov" src="${toy.img}" alt="${toy.name}">
+        </div>
+        <h2>${toy.name}</h2>
+        <p>Price: $${toy.price}</p>
+        `;
+      toyContainer.appendChild(toyCard);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
